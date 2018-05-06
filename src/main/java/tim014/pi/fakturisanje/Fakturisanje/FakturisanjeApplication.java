3d548406ed32c135a96ad2011097c17fd8cfcded
Aleksandar Vasic <coavasic96@gmail.com>
2018-05-06 15:07:40 +0200

    Initial commit

---
+++ src/main/java/tim014/pi/fakturisanje/Fakturisanje/FakturisanjeApplication.java
@@ -0,0 +1,12 @@
+package tim014.pi.fakturisanje.Fakturisanje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakturisanjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakturisanjeApplication.class, args);
+	}
+}
