e9b574eb1b2334fdb481a9888350dc4ff2ac1291
Aleksandar Vasic <coavasic96@gmail.com>
2018-05-07 19:55:21 +0200

    Fix fixing packages

---
+++ src/main/java/tim014/pi/fakturisanje/model/PoslovnaGodina.java
@@ -0,0 +1,60 @@
+package tim014.pi.fakturisanje.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "poslovna_godina")
+public class PoslovnaGodina {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private int godina;
+
+    @Column
+    private boolean zakljucena;
+
+    @OneToMany
+    private List<Faktura> fakture;
+
+    public PoslovnaGodina(int godina, boolean zakljucena) {
+        this.godina = godina;
+        this.zakljucena = zakljucena;
+        this.fakture = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getGodina() {
+        return godina;
+    }
+
+    public void setGodina(int godina) {
+        this.godina = godina;
+    }
+
+    public boolean isZakljucena() {
+        return zakljucena;
+    }
+
+    public void setZakljucena(boolean zakljucena) {
+        this.zakljucena = zakljucena;
+    }
+
+    public List<Faktura> getFakture() {
+        return fakture;
+    }
+
+    public void setFakture(List<Faktura> fakture) {
+        this.fakture = fakture;
+    }
+}
---
+++ src/main/java/tim014/pi/fakturisanje/model/Roba.java
@@ -0,0 +1,77 @@
+package tim014.pi.fakturisanje.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Roba {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String naziv;
+
+    @OneToMany
+    private List<StavkaCenovnika> stavkeCenovnika;
+
+    @OneToMany
+    private List<StavkaFakture> stavkeFakture;
+
+    @ManyToOne
+    @JoinColumn(name = "jedinica_mere")
+    private JedinicaMere jedinicaMere;
+
+    @ManyToOne
+    @JoinColumn(name = "grupa_robe")
+    private GrupaRobe grupaRobe;
+
+    public Roba(String naziv, List<StavkaCenovnika> stavkeCenovnika, List<StavkaFakture> stavkeFakture, JedinicaMere jedinicaMere, GrupaRobe grupaRobe) {
+        this.naziv = naziv;
+        this.stavkeCenovnika = stavkeCenovnika;
+        this.stavkeFakture = stavkeFakture;
+        this.jedinicaMere = jedinicaMere;
+        this.grupaRobe = grupaRobe;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public List<StavkaCenovnika> getStavkeCenovnika() {
+        return stavkeCenovnika;
+    }
+
+    public void setStavkeCenovnika(List<StavkaCenovnika> stavkeCenovnika) {
+        this.stavkeCenovnika = stavkeCenovnika;
+    }
+
+    public List<StavkaFakture> getStavkeFakture() {
+        return stavkeFakture;
+    }
+
+    public void setStavkeFakture(List<StavkaFakture> stavkeFakture) {
+        this.stavkeFakture = stavkeFakture;
+    }
+
+    public JedinicaMere getJedinicaMere() {
+        return jedinicaMere;
+    }
+
+    public void setJedinicaMere(JedinicaMere jedinicaMere) {
+        this.jedinicaMere = jedinicaMere;
+    }
+
+    public GrupaRobe getGrupaRobe() {
+        return grupaRobe;
+    }
+
+    public void setGrupaRobe(GrupaRobe grupaRobe) {
+        this.grupaRobe = grupaRobe;
+    }
+}
