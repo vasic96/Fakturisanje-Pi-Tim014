c644aae5b283a50d417ab596db9291014206bb60
Lazar <Lazar@192.168.109.1>
2018-05-11 10:20:34 +0200

    add Pdv, Stopa Pdv-a, Roba, Grupa Robe Controller-a

---
+++ src/main/java/tim014/pi/fakturisanje/controllers/StopaPdvController.java
@@ -0,0 +1,10 @@
+package tim014.pi.fakturisanje.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value= "api/roba")
+public class StopaPdvController {
+
+}
