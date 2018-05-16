9fb24e9320bf715d2f9f84eadd23c4f8e302a11a
Lazar <Lazar@192.168.109.1>
2018-05-16 20:53:53 +0200

    ispravka imoprta

--- src/main/java/tim014/pi/fakturisanje/controllers/PdvController.java
+++ src/main/java/tim014/pi/fakturisanje/controllers/PdvController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import net.bytebuddy.asm.Advice.Return;
 import tim014.pi.fakturisanje.model.PDV;
 import tim014.pi.fakturisanje.repositories.PdvRepository;
 
