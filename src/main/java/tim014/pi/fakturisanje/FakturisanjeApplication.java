b5a2153ac5893cae479ab3f43193770b393315ea
Aleksandar Vasic <coavasic96@gmail.com>
2018-05-24 16:07:01 +0200

    Sredjen CORS

--- src/main/java/tim014/pi/fakturisanje/FakturisanjeApplication.java
+++ src/main/java/tim014/pi/fakturisanje/FakturisanjeApplication.java
@@ -7,6 +7,17 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class FakturisanjeApplication {
@@ -16,6 +27,27 @@ public class FakturisanjeApplication {
 		return new BCryptPasswordEncoder();
 	}
 
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
+		configuration.setAllowedHeaders(Arrays.asList("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
+		configuration.setAllowCredentials(true);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
+
+
+	@RequestMapping(value= "/api/**", method=RequestMethod.OPTIONS)
+	public void corsHeaders(HttpServletResponse response) {
+		response.addHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+		response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+		response.addHeader("Access-Control-Allow-Headers", "origin, content-type, accept, x-requested-with");
+		response.addHeader("Access-Control-Max-Age", "3600");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FakturisanjeApplication.class, args);
