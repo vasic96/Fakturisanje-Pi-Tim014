9372cd45520799f6614dc5161d5d2284de0bc3d4
lazar996 <lazarmax014@gmail.com>
2018-05-24 10:53:27 +0200

    dodavanje funkcija u Jednica Mere klasi

--- src/main/java/tim014/pi/fakturisanje/controllers/JedinicaMereController.java
+++ src/main/java/tim014/pi/fakturisanje/controllers/JedinicaMereController.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +33,25 @@ public class JedinicaMereController {
 	}
 	
 	
+	@DeleteMapping(value = "delete/{id}")
+	public ResponseEntity<?> deleteJedinicaMere(@PathVariable Long id){
+		
+		if(jedinicaMereRepository.existsById(id)) {
+			jedinicaMereRepository.deleteById(id);
+			
+			return new ResponseEntity<>(HttpStatus.OK);
+		}
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
 	
 
+	
+	@PostMapping(value = "add", consumes="APPLICATION/JSON")
+	public ResponseEntity<JedinicaMere> addJednicaMere(@RequestBody JedinicaMere jedinicaMere){
+		if(jedinicaMere!= null) {
+			
+			return new ResponseEntity<JedinicaMere>(jedinicaMereRepository.save(jedinicaMere), HttpStatus.CREATED);
+		}
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
 }
