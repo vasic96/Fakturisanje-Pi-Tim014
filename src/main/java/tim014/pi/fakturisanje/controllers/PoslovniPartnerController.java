dc814131c5c17c4cd35bd601009673a21d8274e2
ChewDlaka <kerber996@gmail.com>
2018-07-06 00:57:21 +0200

    Preduzece treba da ima ime lolz

--- src/main/java/tim014/pi/fakturisanje/controllers/PoslovniPartnerController.java
+++ src/main/java/tim014/pi/fakturisanje/controllers/PoslovniPartnerController.java
@@ -3,34 +3,52 @@ package tim014.pi.fakturisanje.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tim014.pi.fakturisanje.dto.PoslovniPartnerDTO;
 import tim014.pi.fakturisanje.model.PoslovniPartner;
 import tim014.pi.fakturisanje.repositories.PoslovniPartneriRepository;
 
-import javax.xml.ws.Response;
 import java.util.ArrayList;
 import java.util.List;
 
-@RequestMapping(value = "api/poslovni_partner/")
 @RestController
+@RequestMapping(value = "api/poslovni_partner")
 public class PoslovniPartnerController {
 
     @Autowired
-    private PoslovniPartneriRepository poslovniPartneriRepos;
+    private PoslovniPartneriRepository PPrepo;
 
-    @GetMapping("all")
-    public ResponseEntity<?> sviPartneri(){
-
-        List<PoslovniPartnerDTO> partneriDTO = new ArrayList<>();
-        for (PoslovniPartner partner: poslovniPartneriRepos.findAll()) {
-
-            partneriDTO.add(new PoslovniPartnerDTO(partner));
+    @GetMapping(value = "/all")
+    public ResponseEntity<List<PoslovniPartnerDTO>> dajSvePoslovnePartnere() {
+        List<PoslovniPartnerDTO> pp_repo = new ArrayList<>();
+        for (PoslovniPartner pp : PPrepo.findAll()) {
+            pp_repo.add(new PoslovniPartnerDTO(pp));
         }
-
-        return new ResponseEntity<List<PoslovniPartnerDTO>>(partneriDTO,HttpStatus.OK);
+        return new ResponseEntity<List<PoslovniPartnerDTO>>(pp_repo, HttpStatus.OK);
     }
 
+    @PostMapping(value = "/add", consumes = "APPLICATION/JSON")
+    public ResponseEntity<PoslovniPartner> dodajPoslovnogPartnera(@RequestBody PoslovniPartner pp) {
+        if (pp == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<PoslovniPartner>(PPrepo.save(pp), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/all/{id}")
+    public ResponseEntity<PoslovniPartnerDTO> dajPoslovnogPartneraPoId(@PathVariable Long id) {
+        if (!PPrepo.existsById(id)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<PoslovniPartnerDTO>(new PoslovniPartnerDTO(PPrepo.getOne(id)), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public ResponseEntity<?> izbrisiPoslovnogPartnera(@PathVariable Long id) {
+        if (!PPrepo.existsById(id)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        PPrepo.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
